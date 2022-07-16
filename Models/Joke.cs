bc36f71fef9ce6030e22de7a3fc4b0735b26afc4
mikeydgithub <ampgzo2@gmail.com>
2022-07-16 13:47:01 -0400

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesApp.Models
+{
+    public class Joke
+    {
+        // properties
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+            
+        // method: constructor, going to be empty so other pieces of the code can use it
+        public Joke()
+        {
+
+        }
+    }
+}
